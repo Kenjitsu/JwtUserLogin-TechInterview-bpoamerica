0ff71d585955b814010796a01607b8bd4a8df9ca
Ricardo Corredor <ricorga_97@hotmail.com>
2026-07-23 13:35:21 -0500

    init - initializing project

---
+++ JwtUserLogin.client/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JwtUserLogin.client.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
